a3eb07881631cbb50ebf8023cc19727bda8794c3
Guillaume Chau <alphadelta.fg@gmail.com>
2018-12-30 21:59:54 +0100

    feat(ts): tslint support

--- packages/@nodepack/plugin-typescript/src/app-migrations/index.js
+++ packages/@nodepack/plugin-typescript/src/app-migrations/index.js
@@ -1,15 +1,47 @@
+/** @type {import('@nodepack/service').MigrationPlugin} */
 module.exports = api => {
   api.register({
     id: 'default-template@0.0.1',
     title: 'Template: Render default template',
     // when: api => api.fromVersion('<0.0.1') || api.isFirstInstall,
+    prompts: () => [
+      {
+        name: 'tslint',
+        message: 'Use TSLint',
+        type: 'confirm',
+      },
+    ],
     migrate: (api, options) => {
-      api.render('./templates-0.0.1/default')
+      api.render('./templates-0.0.1/default', options)
       api.move('src/**/*.js', file => `${file.path}${file.name}.ts`)
+
+      if (options.tslint) {
+        api.extendPackage({
+          scripts: {
+            'lint': 'nodepack-service lint',
+          },
+        })
+      }
     },
     rollback: (api, options) => {
       api.unrender('./templates-0.0.1/default')
       api.move('src/**/*.ts', file => `${file.path}${file.name}.js`)
+
+      if (options.tslint) {
+        api.modifyFile('package.json', content => {
+          // @ts-ignore
+          const data = JSON.parse(content)
+          const scripts = data.scripts
+          if (scripts) {
+            for (const key in scripts) {
+              if (scripts[key].match(/nodepack-service lint/)) {
+                scripts[key] = undefined
+              }
+            }
+          }
+          return JSON.stringify(data, null, 2)
+        })
+      }
     },
   })
 
