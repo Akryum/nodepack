67e38c15c019586774a6e38a311f77e4eb4ad52c
Guillaume Chau <alphadelta.fg@gmail.com>
2019-01-22 18:48:03 +0100

    fix(cli): also checks for @nodepack/service in package.json

--- packages/@nodepack/cli/src/util/isInProject.js
+++ packages/@nodepack/cli/src/util/isInProject.js
@@ -1,6 +1,14 @@
 const path = require('path')
 const fs = require('fs-extra')
+const { readPkg } = require('@nodepack/utils')
 
 module.exports = function (cwd) {
-  return fs.existsSync(path.resolve(cwd, '.nodepack')) || fs.existsSync(path.resolve(cwd, 'nodepack.config.js'))
+  return fs.existsSync(path.resolve(cwd, '.nodepack')) ||
+    fs.existsSync(path.resolve(cwd, 'nodepack.config.js')) ||
+    hasDependency(cwd, '@nodepack/service')
+}
+
+function hasDependency (cwd, id) {
+  const pkg = readPkg(cwd)
+  return pkg && (pkg.dependencies[id] || pkg.devDependencies[id])
 }
