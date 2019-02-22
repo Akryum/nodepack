5c88e3476d7aeceb42859014ab769543d9d4767b
Guillaume Chau <guillaume.b.chau@gmail.com>
2019-02-22 10:46:20 +0100

    refactor(app migrator): now search for several paths in plugin

--- packages/@nodepack/app-migrator/src/util/getMigratorPlugins.js
+++ packages/@nodepack/app-migrator/src/util/getMigratorPlugins.js
@@ -1,4 +1,3 @@
-
 const MigratorPlugin = require('../lib/MigratorPlugin')
 const { sortArray } = require('@nodepack/utils')
 const { loadModule } = require('@nodepack/module')
@@ -12,8 +11,20 @@ module.exports = async function (cwd, rawIds) {
   rawIds = sortArray(rawIds, ['@nodepack/service'], true)
   const plugins = []
   for (const id of rawIds) {
-    const apply = loadModule(`${id}/src/app-migrations`, cwd) || (() => {})
-    plugins.push(new MigratorPlugin(id, apply))
+    const files = [
+      `${id}/app-migrations`,
+      `${id}/dist/app-migrations`,
+      `${id}/src/app-migrations`,
+    ]
+    for (const file of files) {
+      try {
+        const apply = loadModule(file, cwd) || (() => {})
+        plugins.push(new MigratorPlugin(id, apply))
+        break
+      } catch (e) {
+        // File not found
+      }
+    }
   }
   return plugins
 }
