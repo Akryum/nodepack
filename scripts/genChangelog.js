653efa047b0ebbf033f38a92c9d0e47bdbc03122
Guillaume Chau <alphadelta.fg@gmail.com>
2019-02-22 11:52:39 +0100

    chore: 0.1.0 changelog [ci skip]

---
+++ scripts/genChangelog.js
@@ -0,0 +1,43 @@
+const fs = require('fs')
+const path = require('path')
+const execa = require('execa')
+const cc = require('conventional-changelog')
+const config = require('@vue/conventional-changelog')
+
+function genNewRelease (version) {
+  return new Promise(resolve => {
+    const newReleaseStream = cc({
+      config,
+      releaseCount: 2,
+      pkg: {
+        transform (pkg) {
+          pkg.version = `v${version}`
+          return pkg
+        },
+      },
+    })
+
+    let output = ''
+    newReleaseStream.on('data', buf => {
+      output += buf
+    })
+    newReleaseStream.on('end', () => resolve(output))
+  })
+}
+
+const gen = (module.exports = async version => {
+  const newRelease = await genNewRelease(version)
+  const changelogPath = path.resolve(__dirname, '../CHANGELOG.md')
+
+  const newChangelog = newRelease + fs.readFileSync(changelogPath, { encoding: 'utf8' })
+  fs.writeFileSync(changelogPath, newChangelog)
+
+  delete process.env.PREFIX
+  await execa('git', ['add', '-A'], { stdio: 'inherit' })
+  await execa('git', ['commit', '-m', `chore: ${version} changelog [ci skip]`], { stdio: 'inherit' })
+})
+
+if (process.argv[2] === 'run') {
+  const version = require('../lerna.json').version
+  gen(version)
+}
