0da9dbc47ff924126bb84adee22969c81626448c
Guillaume Chau <alphadelta.fg@gmail.com>
2019-02-26 10:13:10 +0100

    fix(service): typings

--- packages/@nodepack/service/types/ProjectOptions.d.ts
+++ packages/@nodepack/service/types/ProjectOptions.d.ts
@@ -2,36 +2,36 @@ import Config from 'webpack-chain'
 
 export interface ProjectOptions {
   /** Folder output for prod build */
-  outputDir?: string
+  outputDir?: string;
   /**
-   * Folder containing source.  
-   * By default will be aliased to `@/`  
+   * Folder containing source.
+   * By default will be aliased to `@/`
    * (for example, `@/lib/foo.js` will be resolved as `src/lib/foo.js`).
    */
-  srcDir?: string
+  srcDir?: string;
   /** Entry file (relative to project root) */
-  entry?: string
+  entry?: string;
   /** Enable sourcemaps in production build (can slow down build) */
-  productionSourceMap?: boolean
+  productionSourceMap?: boolean;
   /** Webpack externals packages */
-  externals?: any
+  externals?: any;
   /** Minify production builds */
-  minify?: boolean,
+  minify?: boolean;
   /** Whitelist option for webpack-node-externals */
-  nodeExternalsWhitelist?: any
+  nodeExternalsWhitelist?: any;
   /** Modify webpack config with webpack-chain */
-  chainWebpack?: (config: Config) => void
+  chainWebpack?: (config: Config) => void;
   /** Enable parallel compilation */
-  parallel?: boolean
+  parallel?: boolean;
   /** Force transpile `node_modules` packages with babel */
-  transpileDependencies?: Array.<string | RegExp>
+  transpileDependencies?: Array<string | RegExp>;
   /**
-   * Default port for `process.env.PORT` if it is not defined.  
+   * Default port for `process.env.PORT` if it is not defined.
    * It will change to a free port automatically if it is not available.
    */
-  defaultPort?: number
+  defaultPort?: number;
   /** Enable file linting as part of the build process */
-  lintOnBuild?: boolean
+  lintOnBuild?: boolean;
   /** Options for 3rd-party plugins */
-  pluginOptions?: any
+  pluginOptions?: any;
 }
--- packages/@nodepack/service/types/ServicePlugin.d.ts
+++ packages/@nodepack/service/types/ServicePlugin.d.ts
@@ -4,5 +4,5 @@ import PackPluginApi from '../src/lib/ServicePluginAPI.js'
 type ServicePluginApplyBase = (api: PackPluginApi, options: ProjectOptions) => void
 
 export interface ServicePlugin extends ServicePluginApplyBase {
-  defaultEnvs?: Object.<string, string>
+  defaultEnvs?: Object<string, string>
 }
