12bf219e0ceee487d3af94e6065104204e3b0153
Guillaume Chau <alphadelta.fg@gmail.com>
2019-07-06 21:10:22 +0200

    feat: new openInEditor util

--- packages/@nodepack/utils/src/files.js
+++ packages/@nodepack/utils/src/files.js
@@ -1,3 +1,5 @@
+const launch = require('launch-editor')
+
 /**
  * @param {string} str
  * @returns {string}
@@ -7,4 +9,19 @@ exports.ensureEOL = str => {
     return str + '\n'
   }
   return str
+}
+
+exports.openInEditor = (...args) => {
+  const file = args[0]
+  console.log(`Opening ${file}...`)
+  let cb = args[args.length - 1]
+  if (typeof cb !== 'function') {
+    cb = null
+  }
+  launch(...args, (fileName, errorMessage) => {
+    console.error(`Unable to open '${fileName}'`, errorMessage)
+    console.log(`Try setting the EDITOR env variable. More info: https://github.com/yyx990803/launch-editor`)
+
+    if (cb) cb(fileName, errorMessage)
+  })
 }
