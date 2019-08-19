7e38727ef9c2cef99fa11b13731e2a577b2a12ec
Guillaume Chau <alphadelta.fg@gmail.com>
2019-08-19 14:28:53 +0200

    fix(bootstrap): improved types + optional callback

--- packages/@nodepack/app/src/index.js
+++ packages/@nodepack/app/src/index.js
@@ -1,11 +1,13 @@
 const { createContext, callHook } = require('@nodepack/app-context')
 
 /**
- * @param {(ctx: any) => Promise<any>} callback
+ * @param {(ctx?: any) => Promise<void> | void} callback
  */
-exports.bootstrap = async (callback) => {
+exports.bootstrap = async (callback = null) => {
   const ctx = await createContext()
   await callHook('bootstrap', ctx)
-  await callback(ctx)
+  if (callback) {
+    await callback(ctx)
+  }
   return ctx
 }
