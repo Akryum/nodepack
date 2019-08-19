3d2f9a00c2da1054371778868806ab32af03c014
Guillaume Chau <alphadelta.fg@gmail.com>
2019-08-19 10:00:48 +0200

    fix: version

--- packages/@nodepack/service/src/app-migrations/index.js
+++ packages/@nodepack/service/src/app-migrations/index.js
@@ -14,7 +14,7 @@ module.exports = api => {
           'start': 'node ./dist/app.js',
         },
         dependencies: {
-          '@nodepack/app': '^0.4.2',
+          '@nodepack/app': '^0.4.0',
         },
       })
     },
