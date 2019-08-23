24e528441ac8672999287d3736d2d8d328651bb0
Guillaume Chau <alphadelta.fg@gmail.com>
2019-08-23 18:14:39 +0200

    feat(config): put non-default exports on the config object

--- packages/@nodepack/service/src/runtime/fragments/config.js
+++ packages/@nodepack/service/src/runtime/fragments/config.js
@@ -1,5 +1,14 @@
 const files = require.context('@root', true, /^.\/config\/.*\.[jt]sx?$/)
 for (const key of files.keys()) {
+  const configModule = files(key)
+
+  // Default export
   const [, exportName] = /(\w+)\.[jt]sx?$/.exec(key)
-  module.exports[exportName] = files(key).default
+  module.exports[exportName] = configModule.default
+
+  // Non-default exports
+  for (const secondaryKey of Object.keys(configModule)) {
+    if (secondaryKey === 'default') continue
+    module.exports[secondaryKey] = configModule[secondaryKey]
+  }
 }
