43d547a2ba8d51a618ff553062e5c5f1b25c83eb
Guillaume Chau <alphadelta.fg@gmail.com>
2019-09-30 19:38:50 +0200

    fix: catch errors in app bootstrap

--- packages/@nodepack/app/src/index.js
+++ packages/@nodepack/app/src/index.js
@@ -4,12 +4,16 @@ const { createContext, callHook } = require('@nodepack/app-context')
  * @param {(ctx?: any) => Promise<void> | void} callback
  */
 exports.bootstrap = async (callback = null) => {
-  const ctx = await createContext()
-  await callHook('bootstrap', ctx)
-  if (callback) {
-    await callback(ctx)
+  try {
+    const ctx = await createContext()
+    await callHook('bootstrap', ctx)
+    if (callback) {
+      await callback(ctx)
+    }
+    return ctx
+  } catch (e) {
+    console.error(e)
   }
-  return ctx
 }
 
 exports.printReady = async () => {
