de7e97e76e7fc1f408e9336b7e3767d578597c6b
Guillaume Chau <alphadelta.fg@gmail.com>
2019-10-07 18:52:23 +0200

    fix(types): missing httpServer on ExpressContext

--- packages/@nodepack/plugin-express/types/index.d.ts
+++ packages/@nodepack/plugin-express/types/index.d.ts
@@ -1,7 +1,9 @@
 import { Express, Request, Response } from 'express'
+import { Server } from 'http'
 
 export interface ExpressContext {
   express: Express
+  httpServer: Server
   req: Request
   res: Response
   port: Number
