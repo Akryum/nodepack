76c21f96ec9369a585b9d9eab15fbbe9a812177c
Guillaume Chau <alphadelta.fg@gmail.com>
2019-10-31 18:53:15 +0100

    fix(ts): export MigratorAPI

--- packages/@nodepack/app-migrator/types/index.d.ts
+++ packages/@nodepack/app-migrator/types/index.d.ts
@@ -2,6 +2,7 @@ export { MigrationOptions } from './MigrationOptions'
 export { MigrationPlugin } from './MigrationPlugin'
 export { default as Migrator, MigrationAllOptions } from '../src/lib/Migrator'
 export { default as MigratorPlugin } from '../src/lib/MigratorPlugin'
+export { default as MigratorAPI } from '../src/lib/MigrationAPI'
 export { default as MigrationOperationFile } from '../src/lib/MigrationOperationFile'
 // Utils
 export { default as writeFileTree } from '../src/util/writeFileTree'
