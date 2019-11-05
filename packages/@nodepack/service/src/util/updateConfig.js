f7703b34ad5f5ab571977404215662e3658b85c5
Guillaume Chau <alphadelta.fg@gmail.com>
2019-11-05 23:00:22 +0100

    fix: crash with no-maintenance if no .nodepack/config.json file

--- packages/@nodepack/service/src/util/updateConfig.js
+++ packages/@nodepack/service/src/util/updateConfig.js
@@ -1,10 +1,11 @@
-const { readConfigFile, writeConfigFile } = require('@nodepack/utils')
+const { ensureConfigFolder, readConfigFile, writeConfigFile } = require('@nodepack/utils')
 
 /**
  * @param {string} cwd
  * @param {any} data
  */
 exports.updateConfig = async (cwd, data) => {
+  await ensureConfigFolder(cwd)
   const existingData = await readConfigFile(cwd, 'config.json')
   Object.assign(existingData, data)
   await writeConfigFile(cwd, 'config.json', JSON.stringify(existingData, null, 2))
