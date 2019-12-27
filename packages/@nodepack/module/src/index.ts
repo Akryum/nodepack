3ef72499f589a22ec8f32f7c8cbad314d5137f44
Guillaume Chau <guillaume.b.chau@gmail.com>
2019-12-27 05:43:13 +0100

    fix(module): remove resolve fallback and semver dependency

--- packages/@nodepack/module/src/index.ts
+++ packages/@nodepack/module/src/index.ts
@@ -1,45 +1,7 @@
-import semver from 'semver'
 import { nativeRequire } from './native-require'
-import Module from 'module'
-
 export { nativeRequire } from './native-require'
 
-function resolveFallback (id: string, options?: { paths?: string[] }): string {
-  const isMain = false
-  const fakeParent = new Module('', null)
-
-  const paths = []
-
-  if (options && options.paths) {
-    for (let i = 0; i < options.paths.length; i++) {
-      const path = options.paths[i]
-      // @ts-ignore
-      fakeParent.paths = Module._nodeModulePaths(path)
-      // @ts-ignore
-      const lookupPaths = Module._resolveLookupPaths(id, fakeParent, true)
-
-      if (!paths.includes(path)) paths.push(path)
-
-      for (let j = 0; j < lookupPaths.length; j++) {
-        if (!paths.includes(lookupPaths[j])) paths.push(lookupPaths[j])
-      }
-    }
-  }
-
-  // @ts-ignore
-  const filename = Module._findPath(id, paths, isMain)
-  if (!filename) {
-    const err = new Error(`Cannot find module '${id}'`)
-    // @ts-ignore
-    err.code = 'MODULE_NOT_FOUND'
-    throw err
-  }
-  return filename
-}
-
-const resolve = semver.satisfies(process.version, '>=10.0.0')
-  ? nativeRequire.resolve
-  : resolveFallback
+const resolve = nativeRequire.resolve
 
 function clearRequireCache (id: string, map = new Map<string, boolean>()): void {
   const module = nativeRequire.cache[id]
